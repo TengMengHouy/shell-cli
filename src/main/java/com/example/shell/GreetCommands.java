ffe194e6ddc2b74adafec8c92ff2840027ba12bc
TengMengHouy <la0989975@gmail.com>
2026-03-24 11:53:52 +0700

    first commit

---
+++ src/main/java/com/example/shell/GreetCommands.java
@@ -0,0 +1,4 @@
+package com.example.shell;
+
+public class GreetCommands {
+}
---
+++ src/main/java/com/example/shell/ShellApplication.java
@@ -0,0 +1,28 @@
+package com.example.shell;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.shell.core.ShellRunner;
+import org.springframework.shell.core.command.annotation.Command;
+import org.springframework.shell.core.command.annotation.EnableCommand;
+import org.springframework.shell.core.command.annotation.Option;
+
+@SpringBootApplication
+@EnableCommand(DemoApplication.class)
+public class DemoApplication {
+    public static void main(String[] args) throws Exception {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DemoApplication.class);
+        ShellRunner runner = context.getBean(ShellRunner.class);
+        runner.run(args);
+    }
+
+    @Command(name = "hello", description = "Say hello to a given name", group = "Greetings",
+            help = "A command that greets the user with 'Hello ${name}!'. Usage: hello [-n | --name]=<name>")
+    public void sayHello(@Option(shortName = 'n', longName = "name", description = "the name of the person to greet",
+            defaultValue = "World") String name) {
+        System.out.println("Hello " + name + "!");
+    }
+
+}
